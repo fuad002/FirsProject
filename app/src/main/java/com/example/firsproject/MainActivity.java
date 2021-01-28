ecbbce6e358836b5bad418f6bc2d8396ea9c231b
petik <fuadhidayat2019.@gmail.com>
2021-01-28 10:16:14 +0700

    fuadCommit

--- app/src/main/java/com/example/firsproject/MainActivity.java
+++ app/src/main/java/com/example/firsproject/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 //     finish();
 //                onDestroy();
-                
+
 //                cuma untuk test
             }
         });
